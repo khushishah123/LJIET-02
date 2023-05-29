9bf4c04b5145fbe1269f83222329103e0a45505b
khushishah123 <123561588+khushishah123@users.noreply.github.com>
2023-05-29 21:45:01 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -6,6 +6,7 @@ public class Circle{
         System.out.print("Enter the radius of the circle: ");
         double radius = sc.nextDouble();
         final double PI = 3.14159; 
+        double circumference = 2 * PI * radius;
         double area = PI * radius * radius;
 
         System.out.println("The area of the circle is: " + area);
